e2536dc45a3ad14a1088c6b9df9daa998e397842
Douglas Matos <douglas.matos@adm.unip.br>
2019-05-22 14:27:03 -0300

    adicionado o projeto feito na aula

---
+++ app/src/main/java/com/example/logonrmlocal/jokenpokemon/MenuActivity.kt
@@ -0,0 +1,19 @@
+package com.example.logonrmlocal.jokenpokemon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class MenuActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/logonrmlocal/jokenpokemon/model/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.logonrmlocal.jokenpokemon.model
+
+data class Usuario(
+        val nome: String = "",
+        val email: String = "",
+        val fone: String = ""
+)
